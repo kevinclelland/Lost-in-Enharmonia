a12c2d8d0da54d51922edada816568f79752e17e
Kevin Clelland <kevinclelland@gmail.com>
2020-07-26 19:51:48 +0200

    Fixed modal pop up and checkbox strikethrough.

--- src/js/modal.js
+++ src/js/modal.js
@@ -24,19 +24,17 @@ window.onclick = function(event) {
   }
 }
 
-
-
 //Create strike through
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u2713");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+//var myNodelist = document.getElementsByTagName("LI");
+//var i;
+//for (i = 0; i < myNodelist.length; i++) {
+//  var span = document.createElement("SPAN");
+//  var txt = document.createTextNode("\u2713");
+//  span.className = "close";
+//  span.appendChild(txt);
+//  myNodelist[i].appendChild(span);
+//}
 
 // Click on a close button to hide the current list item
 //var close = document.getElementsByClassName("close");
@@ -49,11 +47,11 @@ for (i = 0; i < myNodelist.length; i++) {
 //}
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('.song');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+//var list = document.querySelector('.song');
+//list.addEventListener('click', function(ev) {
+//  if (ev.target.tagName === 'LI') {
+//    ev.target.classList.toggle('checked');
+//  }
+//}, false);
 
 
